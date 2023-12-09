cc15e546c472d929026c81bc0cdcb31ba73a2810
idul <ludivicobalaterojr@gmail.com>
2023-12-09 20:57:20 +0800

    routes

--- src/index.js
+++ src/index.js
@@ -4,12 +4,19 @@ import "./index.css";
 import UserRegister from "./login/UserRegister";
 import reportWebVitals from "./reportWebVitals";
 import UserLogin from "./login/UserLogin";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <UserRegister />
+    {/* <UserRegister /> */}
     {/* <UserLogin /> */}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/register" element={<UserRegister />} />
+        <Route path="/login" element={<UserLogin />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 

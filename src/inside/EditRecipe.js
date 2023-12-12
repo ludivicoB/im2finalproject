839460a82743a01e687daef5a8bfdd13d9c851a7
Robert <amabarobert@gmail.com>
2023-12-12 21:24:26 +0800

    Edit Ingredient and Recipe

--- src/inside/EditRecipe.js
+++ src/inside/EditRecipe.js
@@ -54,13 +54,13 @@ export default function EditRecipe() {
       <NavBar />
       <Container maxWidth="md">
         <br />
-        <Grid container sx={{ border: '1px solid black' }}>
+        <Grid container sx={{ border: '1px solid black', marginBottom: '2rem' }}>
 
           <Grid container sx={{ border: '1px solid black', padding: '10px', margin: '20px' }}>
             <Grid item xs={12}>
-              <div style={{margin: '20px'}}>
-              <h1 style={{ fontStyle: 'italic', textTransform: 'uppercase' }}>{recipes.recipe_title}</h1>
-              <p style={{ fontSize: '20px' }}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{recipes.recipe_description}</p>
+              <div style={{ margin: '20px' }}>
+                <h1 style={{ fontStyle: 'italic', textTransform: 'uppercase' }}>{recipes.recipe_title}</h1>
+                <p style={{ fontSize: '20px' }}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{recipes.recipe_description}</p>
               </div>
 
 
@@ -69,9 +69,21 @@ export default function EditRecipe() {
               <Grid container>
                 <h3 style={{ marginLeft: '10rem' }}>INGREDIENTS</h3>
                 {ingredients.map((ingredient, index) => (
-                  <Grid item xs={8} key={index} sx={{ border: '1px solid black', margin: '10px auto' }}>
+                  <Grid item xs={10} key={index} sx={{ border: '1px solid black', margin: '10px auto' }}>
                     <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-                      <h3 style={{ marginLeft: '5rem' }}>{ingredient.ingredient_name}</h3>
+
+                      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', alignContent: 'center', marginLeft: '2rem', padding: '0' }}>
+                        <h3 style={{ marginBottom: '0' }}>Name</h3>
+                        <p style={{ fontSize: '20px' }}>{ingredient.ingredient_name}</p>
+                      </div>
+                      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', alignContent: 'center', marginLeft: '-2rem', padding: '0' }}>
+                        <h3 style={{ marginBottom: '0' }}>Quantity</h3>
+                        <p style={{ fontSize: '20px' }}>{ingredient.ingredient_quantity}</p>
+                      </div>
+                      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', alignContent: 'center', marginLeft: '-2rem', padding: '0' }}>
+                        <h3 style={{ marginBottom: '0' }}>Measurement</h3>
+                        <p style={{ fontSize: '20px' }}>{ingredient.ingredient_measurement}</p>
+                      </div>
                       <Link to={`/EditIngredient/${recipe_id.recipeId}/${ingredient.ingredient_id}`}><Button variant="contained" sx={{ display: 'flex', marginRight: '2rem', padding: 0, height: '2rem' }}>Edit</Button></Link>
                     </div>
 
@@ -79,9 +91,9 @@ export default function EditRecipe() {
                 ))}
                 <h3 style={{ marginLeft: '10rem', marginTop: '2rem' }}>INSTRUCTIONS</h3>
                 {instructions.map((instruction, index) => (
-                  <Grid item xs={8} key={index} sx={{ border: '1px solid black', margin: '10px auto' }}>
+                  <Grid item xs={10} key={index} sx={{ border: '1px solid black', margin: '10px auto' }}>
                     <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-                      <h3 style={{ marginLeft: '1rem' }}>{instruction.step_number}: {instruction.step_instruction}</h3>
+                      <p style={{ marginLeft: '1rem', fontSize: '20px' }}><b>{instruction.step_number}</b>: {instruction.step_instruction}</p>
                       <Button variant="contained" sx={{ display: 'flex', marginRight: '2rem', padding: 0, height: '2rem' }}>Edit</Button>
                     </div>
 

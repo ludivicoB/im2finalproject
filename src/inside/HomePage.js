ba6842026ac9f0ebd443fe197ecfde30b8efded6
idul <ludivicobalaterojr@gmail.com>
2023-12-15 00:11:41 +0800

    homepage design

--- src/inside/HomePage.js
+++ src/inside/HomePage.js
@@ -11,6 +11,21 @@ export default function HomePage() {
   return (
     <>
       <NavBar />
+      <div className="homepage">
+        <div className="content">
+          <div>
+            <h1 className="title">Hello {user.firstname}</h1>
+            <h3 className="description">
+              Welcome to LetMeCook, a culinary sanctuary for those who believe
+              in the magic of the kitchen. Discover a world where recipes come
+              to life, and each dish tells a unique tale. LetHimCook is your
+              guide to turning everyday ingredients into extraordinary stories
+              on your plate.
+            </h3>
+          </div>
+          <img src="/images/homepicture.png" className="homepicture" />
+        </div>
+      </div>
     </>
   );
 }
